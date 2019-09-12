426080b73d0b6d314d6d3a70a2c4ad122fdee4c6
dnny60 <dnny60@gmail.com>
2019-09-12 19:25:44 +0800

    chage

--- server1.js
+++ server1.js
@@ -24,7 +24,7 @@ io.on('connection', function(socket){
     //尚有人連接時，向所有用戶發送消息
     socket.on('new-user', user_name =>{
         userNumber[socket.id] = user_name;
-        console.log(userNumber);
+        //console.log(userNumber);
         socket.broadcast.emit('user-connected', user_name);  //所有有連線的人都會收到，但發送者不會
         //io.emit('user-connected', theNmae); 
     });
